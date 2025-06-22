b730247d4a008935b10e397c3d7d20d2d5e35f4d
Vinícius Andrade Gonzato <126361791+Vini0100@users.noreply.github.com>
2025-06-22 19:37:43 -0300

    Revert "feat: Adiciona login e cadastro e interação com"

--- app/auth/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignupCard } from "@/components/SignupCard";
-
-export default function LoginPage() {
-  return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <SignupCard />
-    </div>
-  );
-}
