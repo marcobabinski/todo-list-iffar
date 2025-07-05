2c4fa38832bfa4a59ab3ab318125fc85516f7da9
Marco André Babinski <mab.babinski@gmail.com>
2025-07-05 18:21:48 -0300

    fix: background de pattern em todas as telas

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -2,7 +2,7 @@ import { SignupCard } from "@/components/SignupCard";
 
 export default function LoginPage() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div className="h-screen flex flex-col items-center justify-center bg-pattern">
       <SignupCard />
     </div>
   );
